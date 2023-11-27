991a6502ffffd5d73ec735178709baaef1d4c8a1
Jakub Ivan <xivan@stuba.sk>
2023-11-27 22:29:26 +0100

    header for ik_interface, logging pos and rot
    logging for each joint sample configuration (FK)

---
+++ sensorob_ik_interface/include/sensorob_ik_interface/ik_interface.h
@@ -0,0 +1,15 @@
+//
+// Created by jakub on 27.11.2023.
+//
+
+#ifndef SENSOROB_IK_INTERFACE_IK_INTERFACE_H
+#define SENSOROB_IK_INTERFACE_IK_INTERFACE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+
+std::vector<double> interpolate(double start, double end, int n);
+
+#endif //SENSOROB_IK_INTERFACE_IK_INTERFACE_H
