ddd845b7a96ed6d8fd37abb29df3bd70232dbdb2
Jakub Ivan <xivan@stuba.sk>
2023-11-27 19:38:43 +0100

    ik_interface package

---
+++ sensorob_ik_interface/src/ik_interface.cpp
@@ -0,0 +1,118 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+#include <joint_limits/joint_limits.hpp>
+#include <stdio.h>
+#include <string.h>
+
+// All source files that use ROS logging should define a file-specific
+// static const rclcpp::Logger named LOGGER, located at the top of the file
+// and inside the namespace with the narrowest scope (if there is one)
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("ik");
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto move_group_node = rclcpp::Node::make_shared("ik_interface", node_options);
+
+    // We spin up a SingleThreadedExecutor for the current state monitor to get information
+    // about the robot's state.
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(move_group_node);
+    std::thread([&executor]() { executor.spin(); }).detach();
+
+    static const std::string PLANNING_GROUP = "sensorob_group";
+
+    moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
+
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    const moveit::core::JointModelGroup* joint_model_group =
+            move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+    // Visualization
+    namespace rvt = rviz_visual_tools;
+    moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "base_link", "move_group_tutorial",
+                                                        move_group.getRobotModel());
+    visual_tools.deleteAllMarkers();
+    visual_tools.loadRemoteControl();
+
+    // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
+    Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+    text_pose.translation().z() = 1.0;
+    visual_tools.publishText(text_pose, "MoveGroupInterface_Demo", rvt::WHITE, rvt::XLARGE);
+
+    visual_tools.trigger();
+
+    RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group.getPlanningFrame().c_str());
+
+    RCLCPP_INFO(LOGGER, "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+    RCLCPP_INFO(LOGGER, "Available Planning Groups:");
+    std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
+              std::ostream_iterator<std::string>(std::cout, ", "));
+
+    // Start the demo
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+
+    // Find the joint position limits
+    // Get the active joint models and their bounds
+    std::vector<const moveit::core::JointModel::Bounds*> jmb = joint_model_group->getActiveJointModelsBounds();
+
+    RCLCPP_INFO(LOGGER, "0_max_pos: %f", jmb[0]->data()->max_position_);
+    RCLCPP_INFO(LOGGER, "0_min_pos: %f", jmb[0]->data()->min_position_);
+
+
+
+    // Planning to a joint-space goal
+    moveit::core::RobotStatePtr current_state = move_group.getCurrentState(10);
+    //
+    // Next get the current set of joint values for the group.
+    std::vector<double> joint_group_positions;
+    current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+    // Now, let's modify one of the joints, plan to the new joint space goal, and visualize the plan.
+    joint_group_positions[0] = -0.5;  // radians
+    joint_group_positions[1] -= 0.2;
+
+    bool within_bounds = move_group.setJointValueTarget(joint_group_positions);
+    if (!within_bounds)
+    {
+        RCLCPP_WARN(LOGGER, "Target joint position(s) were outside of limits, but we will plan and clamp to the limits ");
+    }
+
+    // We lower the allowed maximum velocity and acceleration to 5% of their maximum.
+    // The default values are 10% (0.1).
+    // Set your preferred defaults in the joint_limits.yaml file of your robot's moveit_config
+    // or set explicit factors in your code if you need your robot to move faster.
+    move_group.setMaxVelocityScalingFactor(0.05);
+    move_group.setMaxAccelerationScalingFactor(0.05);
+
+    success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+    RCLCPP_INFO(LOGGER, "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
+
+    // Visualize the plan in RViz:
+    visual_tools.deleteAllMarkers();
+    visual_tools.publishText(text_pose, "Joint_Space_Goal", rvt::WHITE, rvt::XLARGE);
+    visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+    visual_tools.trigger();
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to finish ");
+
+    // END_TUTORIAL
+    visual_tools.deleteAllMarkers();
+    visual_tools.trigger();
+
+    rclcpp::shutdown();
+    return 0;
+}
